e4acce113f954a4871a11a3a634884e4694346d3
Brendan Nason <flatrotech@gmail.com>
2022-06-27 21:28:16 -0500

    deal with cache issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is the homepage - cache testing edit???
+          This is the homepage - cache testing edit????
         </p>
         <a
           className="App-link"
