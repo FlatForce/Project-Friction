b0b9b8fd6da2379c85c20097e32e9d1b6d88e3f4
Brendan Nason <flatrotech@gmail.com>
2022-06-28 20:23:51 -0500

    made the switch to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is the homepage - cache testing edit????
+          Edit <code>src/App.tsx</code> and save to reload. TypeScript time!
         </p>
         <a
           className="App-link"
