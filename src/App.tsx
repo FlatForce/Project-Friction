b08a50a679a34f70f3d80b074e21973af3d60b12
knason4 <knason4@gmail@github.com>
2022-07-05 10:31:16 -0500

    commit react-responsive test

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,47 @@ import styles from "./App.module.scss";
 import Home from "./pages/Home/Home"
 import Header from "./components/Header/Header";
 
-class App extends React.Component {
-  render() {
+import {useMediaQuery} from 'react-responsive'
+import { TabletMobile } from "./components/devices/tablet-mobile/tablet-mobile.component";
+import {Desktop} from "./components/devices/desktop/desktop.component"
+import { BigScreen } from './components/devices/big-screen/big-screen.component'
+import { Laptop } from "./components/devices/laptop/laptop.component";
+import { Mobile } from "./components/devices/mobile/mobile.component";
+
+
+
+function App() {
+  const isMobileDevice = useMediaQuery({
+    query: "(min-device-width: 480px)",
+  });
+  
+  const isTabletDevice = useMediaQuery({
+    query: "(min-device-width: 768px)",
+  });
+  
+  const isLaptop = useMediaQuery({
+    query: "(min-device-width: 1024px)",
+  });
+  
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1200px)",
+  });
+
+  const isBigScreen = useMediaQuery({
+    query: "(min-device-width: 1201px)",
+  });
+  
+  
+    
     return (
+      
       <div className={styles.App}>
+        <h1>React Responsive - guide</h1>
+      {isMobileDevice && <Mobile />}
+      {isTabletDevice && <TabletMobile/>}
+      {isDesktop && <Desktop/>}
+      {isLaptop && <Laptop />}
+      {isBigScreen && <BigScreen />}
         <Router>
           <Header />
           <div>
@@ -20,7 +57,7 @@ class App extends React.Component {
         </Router>
       </div>
     );
-  }
+  
 }
 
 export default App;
